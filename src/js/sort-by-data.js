f5415be20178429b39fa976ad94b0c5b32260a80
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-05-07 12:02:09 +0300

    sort -> toSorted

--- src/js/sort-by-data.js
+++ src/js/sort-by-data.js
@@ -1,2 +1,8 @@
+// first example
+// export const sortArrayByDate = array =>
+// 	[...array].sort((a, b) => new Date(a.date) - new Date(b.date));
+
+// second example
+// methode "toSorted" - new immutable methode
 export const sortArrayByDate = array =>
-	[...array].sort((a, b) => new Date(a.date) - new Date(b.date));
+	array.toSorted((a, b) => new Date(a.date) - new Date(b.date));
